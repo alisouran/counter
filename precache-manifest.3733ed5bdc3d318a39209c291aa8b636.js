c4b5bb91c6d4b8062847f578a719ec27fddacfb1
alisouran <alirezasoran@gmai.com>
2019-08-05 01:16:32 +0430

    first commit

---
+++ precache-manifest.3733ed5bdc3d318a39209c291aa8b636.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "963373aad5ce5900e77f8af358b7031a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0283c4965ef6fa8c790d",
+    "url": "/static/css/main.55ed1322.chunk.css"
+  },
+  {
+    "revision": "c40901a4aa654bd7f8bb",
+    "url": "/static/js/2.919f2a3e.chunk.js"
+  },
+  {
+    "revision": "0283c4965ef6fa8c790d",
+    "url": "/static/js/main.42b672e0.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
